ea3b388c81b872d970fecadf8ce3c161c1c48851
Tomoki Kano <tomofuma@gmail.com>
2017-10-19 13:28:26 +0900

    generate random color

--- js/app.jsx
+++ js/app.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {color: this.generateColor()};
+    }
+
+    generateColor(){
+        var hex = '';
+        while(hex.length < 6){
+            hex = hex.concat((Math.round(Math.random()*256)).toString(16));
+        }
+        return hex;
+    }
+
     render() {
         return (
             <div>
-                <h1>App</h1>
+                <h1 style={{color:`#${this.state.color}`}}>{this.state.color}</h1>
             </div>
         );
     }
