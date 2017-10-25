945fc0f641262be1ce5e53ca4d1f5aae7c644def
Tomoki Kano <tomofuma@gmail.com>
2017-10-25 17:20:31 +0900

    load scss

--- js/app.jsx
+++ js/app.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { getColorAttr } from './functions.jsx'
+import {} from './functions.jsx'
+require('../css/index.scss');
 
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {color: this.generateColor()};
+        this.state = {
+            color: this.generateColor()
+        };
     }
 
-    generateColor(){
-        var hex = '';
-        while(hex.length < 6){
-            hex = hex.concat((Math.round(Math.random()*256)).toString(16));
-        }
-        return hex;
+    generateColor() {
+        let color = {
+            hex: null,
+            r: Math.round(Math.random() * 256),
+            g: Math.round(Math.random() * 256),
+            b: Math.round(Math.random() * 256)
+        };
+        color.hex = (color.r.toString(16) + color.g.toString(16) + color.b.toString(16)).toUpperCase();
+        return color;
     }
 
-    newColor(){
+    newColor() {
         this.setState({color: this.generateColor()});
     }
 
     render() {
         return (
-            <div>
-                <h1 style={{color:`#${this.state.color}`}}>{this.state.color}</h1>
-                <p>red: {getColorAttr(this.state.color, 'r')}</p>
-                <p>green: {getColorAttr(this.state.color, 'g')}</p>
-                <p>blue: {getColorAttr(this.state.color, 'b')}</p>
-                <button onClick={()=>this.newColor()}>new color!</button>
+            <div className='mainWrapper'>
+                <h1 style={{
+                    color: `#${this.state.color.hex}`
+                }}>{this.state.color.hex}</h1>
+                <p>red: {this.state.color.r}</p>
+                <p>green: {this.state.color.g}</p>
+                <p>blue: {this.state.color.b}</p>
+                <button onClick={() => this.newColor()}>new color!</button>
             </div>
         );
     }
 }
-ReactDom.render(<App/>,  document.getElementById("app"));
+ReactDom.render(
+    <App/>, document.getElementById("app"));
